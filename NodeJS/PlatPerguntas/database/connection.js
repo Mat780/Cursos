956f5be45d9e76997edc5d504565628a093fef2e
Matheus <46912481+Mat780@users.noreply.github.com>
2021-12-08 17:50:38 -0400

    Delete connection.js

--- NodeJS/PlatPerguntas/database/connection.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('sequelize');
-
-const connection = new sequelize('guia_perguntas', 'root', '@gamerboy10Mf', {
-	host: 'localhost',
-	dialect: 'mysql'
-});
-
-module.exports = connection;
