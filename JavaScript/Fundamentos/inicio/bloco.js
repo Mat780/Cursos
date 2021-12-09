fba6ecef9c01a1b557cb1b6c5f31a0aa1f57340f
Mat780 <mateusfelipe10@live.com>
2021-12-09 10:16:16 -0400

    Bloco de código

---
+++ JavaScript/Fundamentos/inicio/bloco.js
@@ -0,0 +1,30 @@
+{
+	console.log("Passo #1");
+	console.log("Passo #2");
+	console.log("Passo #3");
+}
+
+{
+	console.log("Passo #4");
+	console.log("Passo #5");
+}
+
+{
+	;
+	;
+	;
+}
+
+{
+	{
+		{
+			{
+				{
+					{}
+				}
+			}
+		}
+	}
+}
+
+console.log("Passo Final!");
