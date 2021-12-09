f1553321c958d4ce833a7a5555feeee1f3ce7d17
Mat780 <mateusfelipe10@live.com>
2021-12-09 10:03:55 -0400

    Comentarios

---
+++ JavaScript/Fundamentos/inicio/comentario.js
@@ -0,0 +1,10 @@
+// Console.log é usado para exibir algo no terminal do computador
+console.log("Passo #1"); // Exemplo de comentario pós código
+console.log("Passo #2");
+// console.log("Passo #3"); Comentando um código
+
+/* Comentario
+	Bem
+	Bem
+	Longo
+*/
