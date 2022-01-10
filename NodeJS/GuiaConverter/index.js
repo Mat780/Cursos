00341220a57c6bfe43479d86badaf28f6359c35d
Mat780 <mateusfelipe10@live.com>
2022-01-10 13:04:11 -0400

    179

--- NodeJS/GuiaConverter/index.js
+++ NodeJS/GuiaConverter/index.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
+function modifyUser(name, course, category){		
+	fs.readFile("./usuario.json", {encoding: 'utf8'}, (err, data) => {
+		if(err) throw err;
 
-fs.writeFile('./matheus', "Mudei o conteudo :P", (err) => {
+		let conteudo = JSON.parse(data);
+		conteudo.name = name;
+		conteudo.course = course;
+		conteudo.category = category;
+		console.log(conteudo);
 
-	if(err) {
-		console.log("Aconteceu um erro durante o salvamento");
-	}
+		fs.writeFile('./usuario.json', JSON.stringify(conteudo) , (err, data) => {
+			if(err) throw err;
 
-})
+			console.log("Tudo certo");
+		})
+	})
+}
+
+modifyUser("Matheus", "Formação NodeJs", "NodeJs");
